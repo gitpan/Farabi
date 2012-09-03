97fbe7d42abc3b7b4567afd8c970696a32e1620f
أحمد محمد زواوي - Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2012-09-03 10:32:07 +0000

    Import of AZAWAWI/Farabi-0.01 from CPAN.
    gitpan-cpan-distribution: Farabi
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         AZAWAWI/Farabi-0.01.tar.gz
    gitpan-cpan-author:       AZAWAWI
    gitpan-cpan-maturity:     released

---
+++ lib/Farabi.pm
@@ -0,0 +1,75 @@
+package Farabi;
+use Mojo::Base 'Mojolicious';
+
+use File::Basename 'dirname';
+use File::Spec::Functions 'catdir';
+
+our $VERSION = '0.01';
+
+sub startup {
+	my $app = shift;
+
+	# Use content from directories under lib/Farabi/files
+	
+	$app->home->parse(catdir(dirname(__FILE__), 'Farabi'));
+	$app->static->paths->[0] = $app->home->rel_dir('files/public');
+	$app->renderer->paths->[0] = $app->home->rel_dir('files/templates');
+
+	my $route  = $app->routes;
+	$route->get('/')->to('controller#default');
+	$route->post('/help_search')->to('controller#help_search');
+	$route->post('/perl_tidy')->to('controller#perl_tidy');
+	$route->post('/perl_critic')->to('controller#perl_critic');
+}
+
+1;
+__END__
+
+=pod
+
+=head1 NAME
+
+Farabi - A web-based Perl editor
+
+=head1 SYNOPSIS
+
+  # Run as a seperate Farabi webserver
+  $ farabi daemon
+  
+
+=head1 DESCRIPTION
+
+This is a web-based Perl editor that runs inside your favorite modern
+browser.
+
+please run the following command and it will launch the default browser
+automatically:
+
+  farabi daemon
+
+=head1 SUPPORT
+
+If you find a bug, please report it in:
+
+L<http://code.google.com/p/farabi/issues/list>
+
+If you find this module useful, please rate it in:
+
+L<http://cpanratings.perl.org/d/Farabi>
+
+=head1 SEE ALSO
+
+L<Mojolicious>, L<Mojolicious::Guides>, L<http://mojolicio.us>.
+
+=head1 AUTHOR
+
+Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Ahmad M. Zawawi
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
---
+++ lib/Farabi/Controller.pm
@@ -0,0 +1,130 @@
+package Farabi::Controller;
+use Mojo::Base 'Mojolicious::Controller';
+
+our $VERSION = '0.01';
+
+use Config;
+
+# Taken from Padre::Plugin::PerlCritic
+sub perl_critic {
+	my $self = shift;
+	my $source = $self->param('source');
+
+	my @violations;
+
+	# Check for problems
+	unless ( defined $source ) {
+		$self->render( json => \@violations );
+		return;
+	}
+
+	# Hand off to Perl::Critic
+    require Perl::Critic;
+    @violations = Perl::Critic->new->critique( \$source );
+ 	$self->render( json => \@violations );
+}
+
+# Taken from Padre::Plugin::PerlTidy
+# TODO document it in 'SEE ALSO' POD section
+sub perl_tidy {
+	my $self   = shift;
+	my $source = $self->param('source');
+	
+	my %result = (
+		'error'  => '',
+		'source' => '',
+	);
+
+	# Check for problems
+	unless ( defined $source ) {
+		$self->render( json => \%result );
+		return;
+	}
+
+	my $destination = undef;
+	my $errorfile   = undef;
+	my %tidyargs    = (
+		argv        => \'-nse -nst',
+		source      => \$source,
+		destination => \$destination,
+		errorfile   => \$errorfile,
+	);
+
+	# TODO: suppress the senseless warning from PerlTidy
+	eval {
+		require Perl::Tidy;
+		Perl::Tidy::perltidy(%tidyargs);
+	};
+
+	if ($@) {
+		$result{error} = "PerlTidy Error:\n" . $@;
+	}
+
+	if ( defined $errorfile ) {
+		$result{error} .= "\n$errorfile\n";
+	}
+
+	$result{source} = $destination;
+
+	return $self->render( json => \%result );;
+}
+
+sub help_search {
+	my $self = shift;
+	my $topic = $self->param('topic') // '';
+
+	require File::Spec;
+	my $pod_path = File::Spec->catfile($Config{archlibexp}, 'pods');
+	my $pod_index_filename = 'index.txt';
+	unless ( -f $pod_index_filename ) {
+
+		# Create an index
+		say "Creating POD index";
+		require Pod::Index::Builder;
+		my $p     = Pod::Index::Builder->new;
+		require File::Glob;
+		my @files = File::Glob::glob($pod_path, '*.pod');
+		my $t0    = time;
+		for my $file (@files) {
+			say "Parsing $file";
+			$p->parse_from_file($file);
+		}
+		say "Job took " . ( time - $t0 ) . " seconds";
+		$p->print_index($pod_index_filename);
+	}
+
+	# Search for a keyword in the file-based index
+	require Pod::Index::Search;
+	my $q = Pod::Index::Search->new(
+		filename => $pod_index_filename,
+		filemap  => sub {
+			my $podname = shift;
+			if($podname =~ /^.+::(.+?)$/) {
+				$podname = File::Spec->catfile($pod_path, "$1.pod");
+			}
+			return $podname;
+		}
+	);
+
+	my @results = $q->search($topic);
+	my @help_results;
+	for my $r (@results) {
+		next if $r->podname =~ /perltoc/;
+		push @help_results, {
+			'podname' => $r->podname,
+			'line'    => $r->line,
+			'pod'     => $r->pod,
+		};
+	}
+
+	$self->render( json => \@help_results );
+}
+
+sub default {
+	my $self = shift;
+
+	# Render template "controller/default.html.ep"
+	$self->render;
+}
+
+1;
